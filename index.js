2680397721578aa745cd1d40dbf54181b55ed741
Steven Bruno <sbruno636@gmail.com>
2018-10-05 12:01:07 -0500

    create score logic

--- index.js
+++ index.js
@@ -34,12 +34,40 @@ function playRound(playerSelection, computerSelection) {
             }
         break;
     }
+    
+    updateScore(winner);
 
     switch (winner) {
-        case 0: return `It's a tie! The computer chose ${computerSelection}`;
-        case 1: return `You won! The computer chose ${computerSelection}`;
-        case 2: return `You lost! The computer chose ${computerSelection}`;
+        case 0: return `It's a tie! The computer chose ${computerSelection}.`;
+        case 1: return `You won! The computer chose ${computerSelection}.`;
+        case 2: return `You lost! The computer chose ${computerSelection}.`;
     }
 }
 
 
+function chooseRock() {
+    document.getElementById('result').innerHTML = playRound('Rock', computerPlay())
+}
+
+function choosePaper() {
+    document.getElementById('result').innerHTML = playRound('Paper', computerPlay());
+}
+
+function chooseScissors() {
+    document.getElementById('result').innerHTML = playRound('Scissors', computerPlay())
+}
+
+
+function updateScore(winner) {
+    let playerScore = Number(document.getElementById('playerScore').innerHTML);
+    let cpuScore = Number(document.getElementById('cpuScore').innerHTML);
+
+    if (winner == 1) {
+        playerScore ++;
+        document.getElementById('playerScore').innerHTML = playerScore.toString();
+    }
+    if (winner == 2) {
+        cpuScore ++;
+        document.getElementById('cpuScore').innerHTML = cpuScore.toString();
+    }
+}
